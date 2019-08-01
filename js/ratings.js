22ecbde96e7875029ead5d87dd264ec72bcc171f
Denis Kisina <kisinad@gmail.com>
2019-08-01 19:55:15 +0300

    put dummy data for itenary

---
+++ js/ratings.js
@@ -0,0 +1,20 @@
+var data;
+ function preload() {
+ 	data = loadJSON('js/itinerary.json');
+ }
+
+ function setup(){
+ 	noCanvas();
+	var trips = data.trips;
+	var list = document.querySelector(".test") ;
+	
+	for (let i=0; i<trips.length; i++){
+		var iDiv = document.createElement('LI');
+		iDiv.className = 'block';
+		iDiv.textContent = trips[i].itinerary
+		list.appendChild(iDiv);
+		
+		
+		console.log(iDiv.textContent);
+	}	
+ }
