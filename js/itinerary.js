5ce3da1202ede58985b3516378c2183ff10f2c05
Denis Kisina <kisinad@gmail.com>
2019-08-02 15:04:47 +0300

    added jquery to cappture user data

--- js/itinerary.js
+++ js/itinerary.js
@@ -6,10 +6,18 @@ $(document).ready(function()
         let email = $("#email").val();
         let startdate = $("#startdate").val();
         let enddate = $("#enddate").val();
-        let people = $("#people").val();
-        let gender = $("#gender").val();
+        let people = $('option').closest('select').text();
+        let fgender = $('input').closest('label').text();
+        let mgender = $('input').closest('label').text();
         // let email = $("#email").val();
-console.log(firstname);
+        // $("gettrips").trigger("reset");
+        console.log(fgender);
+        console.log(mgender);
+        console.log(people);
         
 });
+// $('[name="fname"]').change(function() {
+//     var fgender = $('input').closest('label').text();
+//     alert(fgender);
+// });
 });
