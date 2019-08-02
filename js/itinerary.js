6cc79d1f044288b0c43f5ffa2dbb325c34102883
Denis Kisina <kisinad@gmail.com>
2019-08-02 10:54:28 +0300

    added jquery file to captured user input

---
+++ js/itinerary.js
@@ -0,0 +1,15 @@
+$(document).ready(function()
+{
+    $("#gettrips").click(function(){
+        let firstname = $("#firstname").val();
+        let surname = $("#surname").val();
+        let email = $("#email").val();
+        let startdate = $("#startdate").val();
+        let enddate = $("#enddate").val();
+        let people = $("#people").val();
+        let gender = $("#gender").val();
+        // let email = $("#email").val();
+console.log(firstname);
+        
+});
+});
