823342baf6086824f580017925ea990e5ab992dd
Denis Kisina <kisinad@gmail.com>
2019-08-03 13:05:47 +0300

    added sorting from highest to lowest & specified to display top 3

--- js/ratings.js
+++ js/ratings.js
@@ -4,18 +4,32 @@ var data;
  }
 
  function setup(){
- 	noCanvas();
+	 noCanvas();
+	 
 	var trips = data.trips;
 	var list = document.querySelector(".test") ;
 	
-	for (let i=0; i<trips.length; i++){
-		var iDiv = document.createElement('UL');
-		iDiv.className = 'block';
-		iDiv.textContent = trips[i].itinerary
-		list.appendChild(iDiv);
+	// for (let i=0; i<trips.length; i++){
+	// 	var iDiv = document.createElement('UL');
+	// 	iDiv.className = 'block';
+	// 	iDiv.textContent = trips[i].itinerary;
+	// 	// iDiv.textContent = trips.sort((a, b) => (b.rating)-(a.rating));
+	// 	list.appendChild(iDiv);
 		
 		
 		// console.log(iDiv.textContent);
-	}	
- }
+		
+	let arraySorted = trips.sort((a, b) => (b.rating)-(a.rating))
+	for (let i =0; i<3;i++){
+		console.log(arraySorted[i].itinerary);
+		var iDiv = document.createElement('UL');
+		iDiv.className = 'block';
+		iDiv.textContent = arraySorted[i].itinerary;
+		// iDiv.textContent = trips.sort((a, b) => (b.rating)-(a.rating));
+		list.appendChild(iDiv);
+		
+	}
+	}
+		
+//  }
 // Rating stars JS//
