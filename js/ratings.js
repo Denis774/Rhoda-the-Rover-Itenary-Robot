a4806a22b2bb55ec0572f4f89d6259499f37ae60
Denis Kisina <kisinad@gmail.com>
2019-08-03 09:41:46 +0300

    put back the js to display top rated trips'

--- js/ratings.js
+++ js/ratings.js
@@ -1,21 +1,21 @@
-// var data;
-//  function preload() {
-//  	data = loadJSON('js/itinerary.json');
-//  }
+var data;
+ function preload() {
+ 	data = loadJSON('js/itinerary.json');
+ }
 
-//  function setup(){
-//  	noCanvas();
-// 	var trips = data.trips;
-// 	var list = document.querySelector(".test") ;
+ function setup(){
+ 	noCanvas();
+	var trips = data.trips;
+	var list = document.querySelector(".test") ;
 	
-// 	for (let i=0; i<trips.length; i++){
-// 		var iDiv = document.createElement('UL');
-// 		iDiv.className = 'block';
-// 		iDiv.textContent = trips[i].itinerary
-// 		list.appendChild(iDiv);
+	for (let i=0; i<trips.length; i++){
+		var iDiv = document.createElement('UL');
+		iDiv.className = 'block';
+		iDiv.textContent = trips[i].itinerary
+		list.appendChild(iDiv);
 		
 		
-// 		// console.log(iDiv.textContent);
-// 	}	
-//  }
-// // Rating stars JS//
+		// console.log(iDiv.textContent);
+	}	
+ }
+// Rating stars JS//
